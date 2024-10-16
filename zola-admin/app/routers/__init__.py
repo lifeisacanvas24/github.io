e1863976534f8b795c6429eaec81c70e84d41a46
lifeisacanvas24 <agriyain2k@gmail.com>
2024-10-16 12:10:19 +0530

    removed the earlier zola-admin

--- zola-admin/app/routers/__init__.py
+++
@@ -1,6 +0,0 @@
-# app/routers/__init__.py
-
-# This file can be empty, or you can include router imports here if needed.
-
-from .user import router as user_router
-from .admin import router as admin_router
